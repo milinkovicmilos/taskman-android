350a727076d80451be6798023865d443e49f2ac8
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-12 15:34:39 +0200

    Reading configuration file

--- Client/MauiProgram.cs
+++ Client/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Client;
 
@@ -7,18 +9,28 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
+        builder.UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
 #if DEBUG
         builder.Logging.AddDebug();
+        const string ConfigFileName = "Client.Config.appsettings.Development.json";
+#else
+        const string ConfigFileName = "Client.Config.appsettings.Production.json";
 #endif
+        var a = Assembly.GetExecutingAssembly();
+        Console.WriteLine(a.GetName().Name);
+        using var stream = a.GetManifestResourceStream(ConfigFileName);
+        if (stream != null)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonStream(stream).Build();
+            builder.Configuration.AddConfiguration(configuration);
+        }
 
+        builder.Services.AddSingleton<MainPage>();
         return builder.Build();
     }
 }
