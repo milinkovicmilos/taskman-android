22b3ad9ca14d79723d143db16dbbde3334ff4ab7
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-14 19:25:35 +0200

    Service for fetching projects

---
+++ Client/Services/ProjectsService.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Client.Model;
+using Client.Model.Response;
+
+namespace Client.Services;
+
+public class ProjectsService
+{
+    private readonly HttpClientService _http;
+    private JsonSerializerOptions _options;
+
+    public ProjectsService(HttpClientService httpClient)
+    {
+        _http = httpClient;
+        _options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+    }
+
+    public async Task<PaginatedResponse<ProjectSummary>> FetchProjects(int page = 1)
+    {
+        var response = await _http.GetAsync($"api/projects?page={page}");
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        var result = JsonSerializer.Deserialize<PaginatedResponse<ProjectSummary>>(jsonResponse, _options);
+        if (result is null)
+            throw new Exception("Failed to get projects");
+
+        return new PaginatedResponse<ProjectSummary>();
+    }
+}
