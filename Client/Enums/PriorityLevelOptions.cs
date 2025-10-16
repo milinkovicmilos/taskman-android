69a8ac99fca9e1df5a2048bd41742750bc3a47a0
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-16 15:10:33 +0200

    Priotiy level options enum

---
+++ Client/Enums/PriorityLevelOptions.cs
@@ -0,0 +1,16 @@
+namespace Client.Enums;
+
+public enum PriorityLevelOptions
+{
+    Unset,
+    Level1,
+    Level2,
+    Level3,
+    Level4,
+    Level5,
+    Level6,
+    Level7,
+    Level8,
+    Level9,
+    Level10,
+}
