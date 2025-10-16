b689b90fb44fa03b2eea609e4acdeb183691eeb2
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-16 15:18:55 +0200

    Handling task creation request

--- Client/Services/TasksService.cs
+++ Client/Services/TasksService.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Client.Model.Response;
 using Client.Model.Tasks;
 
@@ -26,6 +28,25 @@ public class TasksService
         var result = JsonSerializer.Deserialize<PaginatedResponse<TaskSummary>>(jsonResponse, _options);
         if (result is null)
             throw new Exception("Failed to fetch projects tasks");
+
+        return result;
+    }
+
+    public async Task<MessageDataResponse<CreateDataResponse?>> StoreTaskAsync(int projectId, CreateTaskRequest request)
+    {
+        var jsonOptions = new JsonSerializerOptions(_options)
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+        var json = JsonSerializer.Serialize(request, jsonOptions);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _http.PostAsync($"api/projects/{projectId}/tasks", content);
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        var result = JsonSerializer.Deserialize<MessageDataResponse<CreateDataResponse?>>(jsonResponse, _options);
+        if (result is null)
+            throw new Exception("Failed to store task");
 
         return result;
     }
