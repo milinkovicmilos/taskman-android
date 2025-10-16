746d3d583239c4c0cc166e00762da4804de6f25e
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-16 19:57:59 +0200

    Task details card view for task details page

---
+++ Client/View/Tasks/TaskDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.View.Tasks;
+
+public partial class TaskDetailsView : ContentView
+{
+    public TaskDetailsView()
+    {
+        InitializeComponent();
+    }
+}
